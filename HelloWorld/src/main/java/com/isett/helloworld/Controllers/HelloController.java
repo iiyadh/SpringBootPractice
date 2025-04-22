959659c446cebfbea34acc18435f03a9355a46bb
Tabai Iyadh <tabaiiyadh317@gmail.com>
2025-04-23 00:06:36 +0100

    Lab 1 SpringBoot Module 1 - Introduction & Environment Setup

---
+++ HelloWorld/src/main/java/com/isett/helloworld/Controllers/HelloController.java
@@ -0,0 +1,33 @@
+package com.isett.helloworld.Controllers;
+
+import com.isett.helloworld.Models.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.concurrent.atomic.AtomicLong;
+
+
+@RestController
+public class HelloController {
+
+    private static final String TEMPLATE = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "Welcome to Spring Boot!";
+    }
+
+    @GetMapping("/api/greeting")
+    public Greeting greetingJson(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(TEMPLATE,name));
+    }
+}
