d11a9a31529519cb4f25017539a9cf8329fc2461
Tabai Iyadh <tabaiiyadh317@gmail.com>
2025-04-30 00:29:26 +0100

    Lab5 Redis Basic

---
+++ Lab5/src/test/java/com/example/bookredis/BookRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
